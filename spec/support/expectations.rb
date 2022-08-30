b4d4d3aa2aed5286b351dc4dac18f16344539c79
Mathias H Steffensen <ms@driversnote.com>
2022-08-30 14:16:36 +0200

    A little work for when we will run specs with emerald itself

--- spec/support/expectations.rb
+++ spec/support/expectations.rb
@@ -1,9 +1,29 @@
 module Expectations
     class Expectation
+        attr_reader(:matcher)
+
         def initialize(matcher)
             @matcher = matcher
         end
+
+        def matches?(expected)
+            matcher.call(expected)
+        end
     end
 
-    def eq; end
+    class EqualityMatcher
+        attr_reader(:actual)
+
+        def initialize(actual)
+            @actual = actual
+        end
+
+        def call(expected)
+            expected == actual
+        end
+    end
+
+    def eq(actual)
+        Expectation.new(EqualityMatcher.new(actual))
+    end
 end
